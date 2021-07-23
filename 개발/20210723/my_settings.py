16561cb69577305505812077b317d9ed282f31b1
ehs2803 <hr2803@naver.com>
2021-07-23 21:34:38 +0900

    20210723

---
+++ 개발/20210723/my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default':{
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'task_manager',
+        'USER' : 'root',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+    }
+}
+SECRET_KEY = 'fnibvn(cnb-rz7f@g4dwwmokz#cv1(iva)+w&8ca_b8-=+mb2^'
